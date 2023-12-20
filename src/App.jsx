6fda45029fd726271a5bf334074a436137697fd8
AdriVelasco2 <isadri35@hotmail.com>
2023-12-20 12:20:32 +0100

    pruebas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css'
 import Inicio from './components/Inicio.jsx'
-import {Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Sets3 from './components/Sets3.jsx'
 import Pruebas from './components/Pruebas.jsx'
 function App() {
 
   return (
     <>
+<Router>
 <Routes>
 <Route path="/" element={<Inicio />} />    
 <Route path="/marcador" element={<Sets3 />} />
 <Route path="/pruebas" element={<Pruebas />} />
-</Routes>
-    
+       
+    </Routes>
+    </Router>
     </>
   )
 }
